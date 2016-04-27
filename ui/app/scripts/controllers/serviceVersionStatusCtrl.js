3117da8e50b04b1bdbfe02e1856f3c1414108a12
Roi Rav-Hon <roi@logz.io>
2016-04-27 18:16:02 +0300

    Fix gilly comments

--- ui/app/scripts/controllers/serviceVersionStatusCtrl.js
+++ ui/app/scripts/controllers/serviceVersionStatusCtrl.js
@@ -47,7 +47,7 @@ angular.module('apollo')
             });
 
             apolloApiService.getAllUsers().then(function(response) {
-``
+
                 var tempUsers = {};
 
                 response.data.forEach(function(user) {
